6e6c22d6bf0c788dded2e3898a43c9a4a9c5a4e2
PVDung <pvdung.sd@gmail.com>
2020-06-07 12:27:55 +0700

    Hoan thanh login

--- InternetCafeClient/FormLogin.cs
+++ InternetCafeClient/FormLogin.cs
@@ -39,6 +39,8 @@ namespace InternetCafeClient
                     usernameHandler += i;
             }
             realpass = AcceptLogin(usernameHandler, pass);
+            if (GetInfo(usernameHandler) == 0)
+                realpass = "2";
             Console.WriteLine(realpass);
             if (realpass == "1")
             {
@@ -46,6 +48,8 @@ namespace InternetCafeClient
                 timingForm = new FormTiming(userTxtBx.Text, pass);
                 timingForm.Show();
             }
+            else if(realpass=="2")
+                MessageBox.Show("Tài khoản không đủ tiền", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
             else if(realpass.Equals("error"))
                 MessageBox.Show("Không nhận phản hồi từ server", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
             else
@@ -95,13 +99,22 @@ namespace InternetCafeClient
         {
             pass = EncryptPassword(passTxtBx.Text);
         }
-        //ok?okđm vừa làm cái attt  nhìn chán vc đéo biết phải làm lại k
 
+        private int GetInfo(string username)
+        {
+            //tao ket noi
+            SckClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //tao cong
+            ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            //bat dau gui du lieu
+            SckClient.SendTo(Encoding.ASCII.GetBytes("2" + username + " " + Dns.GetHostName()), ep);
+            // xu ly du lieu nhan duoc
+            int size = SckClient.ReceiveFrom(data, 0, 1024, SocketFlags.None, ref ep);
+            string result = Encoding.ASCII.GetString(data, 0, size);
+            int money = int.Parse(result);
+            return money;
+        }
     }
 }
-// khác nhau ở chỗ lồn nào ? :))) ddc ko?
-// m thử test lại xem chạy đi ???
-//đang ở branch master
-// t vừa merge vào rồi
-// thử push r
+
 
