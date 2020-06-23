570739bc0b97be2e7c6cb24918fda45cde5e01c0
PVDung <pvdung.sd@gmail.com>
2020-06-23 09:14:14 +0700

    finish

--- InternetCafeClient/FormInfo.cs
+++ InternetCafeClient/FormInfo.cs
@@ -12,7 +12,25 @@ namespace InternetCafeClient
 {
     public partial class FormInfo : Form
     {
+        private string un;
+        private string cmnd;
+        private string ns;
+        private string sdt;
+        private int money;
+
         public FormInfo(string un,string cmnd,string ns,string sdt,int money)
+        {
+            this.un = un;
+            this.cmnd = cmnd;
+            this.ns = ns;
+            this.sdt = sdt;
+            this.money = money;
+            InitializeComponent();
+            Changeinf();
+
+        }
+
+        private void Changeinf()
         {
             textBox1.Text = un;
             textBox2.Text = cmnd;
